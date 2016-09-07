23f5be733e18f89210bc40a9a62dfbc2a6c52fe5
madsr <madsr@10.50.138.81>
2016-09-07 10:34:41 +0200

    Test

---
+++ CA-1/src/main/java/CA/UsersService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CA;
+
+import javafx.beans.Observable;
+
+/**
+ *
+ * @author Mathias
+ */
+public class UsersService extends Thread implements Observable
+{
+  
+  public int usersOnline;
+  
+  public void run()
+  {
+    while(true)
+    {
+      TCPServer.users.size();
+    }
+  }
+  
+}
